f2cb95eada057cd9de8aa39a51cc07b31be6d487
Saleh Zaidan <saleh.zaidan@cadit.com>
2026-02-02 14:07:03 +0700

    Implement model, repository, and config

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,131 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import z from "zod";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "twls" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand("twls.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from twls!");
-  });
-
-  context.subscriptions.push(disposable);
+  const model = new Model(vscode.workspace.workspaceFolders);
+  context.subscriptions.push(model);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate(): void {}
+
+class Model implements vscode.Disposable {
+  private repositories = new Map<string, Repository>();
+  private configWatcher;
+
+  constructor(folders: readonly vscode.WorkspaceFolder[] | undefined) {
+    folders?.forEach((folder) => {
+      this.addRepository(folder.uri).catch((error) => {
+        console.error(error);
+      });
+    });
+
+    this.configWatcher = vscode.workspace.createFileSystemWatcher(
+      Config.globPattern,
+    );
+    this.configWatcher.onDidCreate((e) => {
+      this.addRepository(Config.getRootUri(e)).catch((error) => {
+        console.error(error);
+      });
+    });
+    this.configWatcher.onDidChange((e) => {
+      this.addRepository(Config.getRootUri(e)).catch((error) => {
+        console.error(error);
+      });
+    });
+    this.configWatcher.onDidDelete((e) => {
+      this.removeRepository(Config.getRootUri(e));
+    });
+  }
+
+  dispose(): void {
+    this.repositories.clear();
+
+    this.configWatcher.dispose();
+  }
+
+  private async addRepository(rootUri: vscode.Uri): Promise<void> {
+    let config: Config | undefined;
+
+    try {
+      config = await Config.load(rootUri);
+    } catch (error) {
+      if (error instanceof vscode.FileSystemError) {
+        return;
+      }
+
+      throw error;
+    }
+
+    const key = rootUri.toString();
+    let repo = this.repositories.get(key);
+
+    if (repo) {
+      this.repositories.delete(key);
+    }
+
+    repo = new Repository(config);
+    this.repositories.set(key, repo);
+
+    console.log(
+      `[Model][addRepository] Repository added: ${rootUri.toString()}`,
+    );
+  }
+
+  private removeRepository(rootUri: vscode.Uri): void {
+    const key = rootUri.toString();
+    this.repositories.delete(key);
+
+    console.log(
+      `[Model][removeRepository] Repository removed: ${rootUri.toString()}`,
+    );
+  }
+}
+
+class Repository {
+  constructor(private config: Config) {}
+
+  pull(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  push(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class Config {
+  private static FOLDER_NAME = ".twls";
+  private static THINGWORX_FILE_NAME = "thingworx.json";
+
+  private static thingworxSchema = z.object({
+    baseUrl: z.url(),
+    appKey: z.guid(),
+  });
+
+  constructor(
+    private rootUri: vscode.Uri,
+    public baseUrl: string,
+    public appKey: string,
+  ) {}
+
+  static get globPattern(): vscode.GlobPattern {
+    return `**/${this.FOLDER_NAME}/${this.THINGWORX_FILE_NAME}`;
+  }
+
+  static getRootUri(thingworxUri: vscode.Uri): vscode.Uri {
+    return vscode.Uri.joinPath(thingworxUri, "..", "..");
+  }
+
+  static async load(rootUri: vscode.Uri): Promise<Config> {
+    const content = await vscode.workspace.fs.readFile(
+      vscode.Uri.joinPath(
+        rootUri,
+        Config.FOLDER_NAME,
+        Config.THINGWORX_FILE_NAME,
+      ),
+    );
+    const json = JSON.parse(new TextDecoder().decode(content)) as unknown;
+    const parsed = this.thingworxSchema.parse(json);
+    return new Config(rootUri, parsed.baseUrl, parsed.appKey);
+  }
+}
